61ab4b42ee721347a3a064e6c724172062229e9e
Siddharth S <siddharthsoma01@gmail.com>
2022-08-20 17:45:01 +0530

    Made separate files for each Endpoint
    Each endpoint will now be acting as mini apps reporting to the main app.get() function as and when the corresponding endpoint is called.

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
-//here we make the API
+//Here we make the API
+//====================
 
 //express for creating backend application
 const express = require("express");
 
-//used to make API requests
-const request = require("request-promise");
-
-//importing the dotenv file for fetching the secure url for connection to database
+//importing the dotenv file for storing the important keys
 const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-dotenv.config();
+//===============================================================================
 const PORT = process.env.PORT || 5000; // PORT for deploying the application
+//===============================================================================
 
 const baseUrl = `http://api.scraperapi.com?api_key=${process.env.API_KEY}&autoparse=true`;
+exports.baseUrl = baseUrl; //exporting this variable
+
+//======================================================================================
 
 //helps parse json input
 app.use(express.json());
+
+//END POINTS
+const productRoute = require("./routes/product");
 
 //every express application needs a route.
 app.get("/", (req, res) => {
     res.send("Welcome to AMAZON Scraper API");
 });
 
-//GET Product Details
-// : indicates that productId is dynamic
-//async (req , res) is a callback function
-app.get("/products/:productId", async (req, res) => {
-    //get Productid from parameters
-    //productId is going to be populated in req.params
-    const { productId } = req.params;
+app.use("/products", productRoute);
 
-    //try-catch block
-    try {
-        //response from ScraperAPI
-        const response = await request(
-            `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
-        );
 
-        //send response back from our server
-        res.json(response);
-    } catch (err) {
-        res.json(err);
-    }
-});
-
+//====================================================================================
 //We need to make our server listen on a specific port
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
---
+++ routes/product.js
@@ -0,0 +1,36 @@
+//ROUTE 1 - GET Product Details
+//---------------------------------
+const router = require("express").Router();
+//used to make API requests
+const request = require("request-promise");
+//importing the "baseUrl" variable from index.js file
+const { baseUrl } = require("../index");
+
+console.log(baseUrl); //fetching the baseUrl variable from index.js file
+// console.log(`${ind.baseUrl}&url=https://www.amazon.com/dp/B09DGRPM53`)
+
+// : indicates that productId is dynamic
+//async (req , res) is a callback function
+
+router.get("/:productId", async (req, res) => {
+    //get Productid from parameters
+    //productId is going to be populated in req.params
+    const { productId } = req.params;
+
+    //try-catch block
+    try {
+        //response from ScraperAPI
+        const response = await request(
+            `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+        );
+
+        //send response back from our server
+        res.json(JSON.parse(response));
+        //Gives it in JSON format. INSTALL JSON formatter extension IN WEB browser for the same!
+        //Makes json look prettier
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+module.exports = router;
