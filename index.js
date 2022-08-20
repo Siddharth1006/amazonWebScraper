816c26f8d230fb21c1398e3629841e1e794c9f5b
Siddharth S <siddharthsoma01@gmail.com>
2022-08-20 15:58:49 +0530

    Created /products/:productId endpoint.
    Also added .env file for obvious reasons

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const express = require("express");
 //used to make API requests
 const request = require("request-promise");
 
+//importing the dotenv file for fetching the secure url for connection to database
+const dotenv = require("dotenv");
+
 const app = express();
+
+dotenv.config();
 const PORT = process.env.PORT || 5000; // PORT for deploying the application
+
+const baseUrl = `http://api.scraperapi.com?api_key=${process.env.API_KEY}&autoparse=true`;
 
 //helps parse json input
 app.use(express.json());
@@ -17,5 +24,27 @@ app.get("/", (req, res) => {
     res.send("Welcome to AMAZON Scraper API");
 });
 
+//GET Product Details
+// : indicates that productId is dynamic
+//async (req , res) is a callback function
+app.get("/products/:productId", async (req, res) => {
+    //get Productid from parameters
+    //productId is going to be populated in req.params
+    const { productId } = req.params;
+
+    //try-catch block
+    try {
+        //response from ScraperAPI
+        const response = await request(
+            `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+        );
+
+        //send response back from our server
+        res.json(response);
+    } catch (err) {
+        res.json(err);
+    }
+});
+
 //We need to make our server listen on a specific port
-app.listen(PORT , () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
